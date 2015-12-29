eebca6f6e877b1a42ff2f505085cf248f63c8bd3
freejacque <freejacque@gmail.com>
2015-12-29 18:40:39 -0500

    comment explaining the test

--- keyword-wrangler/spec/e2e/apiSpec.js
+++ keyword-wrangler/spec/e2e/apiSpec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//  test that the test infrastructure works
+// ./node_modules/.bin/jasmine-node --verbose ./spec/
 describe('The API', function() {
 
   it('should just work', function() {
