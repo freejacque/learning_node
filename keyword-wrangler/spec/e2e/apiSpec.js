902d3d5f88146e4fe08530404d05651dbc1a51e2
freejacque <freejacque@gmail.com>
2015-12-30 18:19:32 -0500

    added expected DB content to spec assertion

--- keyword-wrangler/spec/e2e/apiSpec.js
+++ keyword-wrangler/spec/e2e/apiSpec.js
@@ -7,6 +7,14 @@ describe('The API', function() {
   //  tests for correct server response
   //  ./node_modules/.bin/jasmine-node --verbose --captureExceptions ./spec/
   it('should respond to a GET request at /api/keywords/', function(done) {
+    var expected = {
+      "_items": [
+        {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+        {'id': 2, 'value': 'Onion', 'categoryID': 1},
+        {'id': 3, 'value': 'Knife', 'categoryID': 2}
+      ]
+    };
+
     request.get(
     {
       'url' : 'http://localhost:8080/api/keywords/',
