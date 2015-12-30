36161f4f544e4a164eb25618586c690e559b387a
freejacque <freejacque@gmail.com>
2015-12-30 18:39:22 -0500

    created dbWrapper module

--- keyword-wrangler/src/backend/dbSession.js
+++ keyword-wrangler/src/backend/dbSession.js
@@ -1,1 +1,9 @@
 'use strict';
+
+var DBWrapper = require('node-dbi').DBWrapper;
+
+var dbWrapper = new DBWrapper('sqlite3', {'path': '/var/tmp/keyword-wrangler.test.sqlite'});
+
+dbWrapper.connect();
+
+module.exports = dbWrapper;
