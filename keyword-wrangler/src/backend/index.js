4907287e36a68386a5498504544edaf0a7cd63ca
freejacque <freejacque@gmail.com>
2015-12-30 18:08:47 -0500

    created a server

--- keyword-wrangler/src/backend/index.js
+++ keyword-wrangler/src/backend/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var Percolator = require('percolator').Percolator;
+
+var port = 8080;
+var server = Percolator({'port': port});
+
+server.route('/api/keywords',
+  {
+    GET: function(request, response) {
+      response.object({'foo': 'bar'}).send()
+    }
+  }
+);
+
+server.listen(function() {
+  console.log('Server started and listening on port', port);
+});
