a58ef8f5200017efd0fabc97fcd431ba9d8f2f96
freejacque <freejacque@gmail.com>
2015-12-31 23:25:20 -0500

    added autolink param

--- keyword-wrangler/src/backend/index.js
+++ keyword-wrangler/src/backend/index.js
@@ -4,7 +4,7 @@ var Percolator = require('percolator').Percolator;
 var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
-var server = Percolator({'port': port});
+var server = Percolator({'port': port, 'autolink': false});
 
 server.route('/api/keywords',
   {
