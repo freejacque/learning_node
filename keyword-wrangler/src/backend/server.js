a733aeabf82944c51849a87b8a3295eda3dfdcbc
freejacque <freejacque@gmail.com>
2015-12-31 23:49:39 -0500

    completed the server

--- keyword-wrangler/src/backend/server.js
+++ keyword-wrangler/src/backend/server.js
@@ -7,6 +7,22 @@ var Server = function(port) {
   var server = Percolator({'port': port, autoLink: false});
 
   server.route('/api/keywords',
+    {
+      GET: function(request, res) {
+        dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id',
+          function(err, rows) {
+            if(err) {
+              console.log(err);
+              res.status.internalServerError(err);
+            } else {
+              res.collection(rows).send();
+            }
+          });
+      }
+    }
+  );
 
-  )
-}
+  return server;
+};
+
+module.exports = {'Server': Server};
