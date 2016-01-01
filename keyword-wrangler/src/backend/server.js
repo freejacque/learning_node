39942fbd171065d8309ed175e9905b5b3ef2ff35
freejacque <freejacque@gmail.com>
2015-12-31 23:45:56 -0500

    started server function

--- keyword-wrangler/src/backend/server.js
+++ keyword-wrangler/src/backend/server.js
@@ -2,3 +2,11 @@
 
 var Percolator = require('percolator').Percolator;
 var dbSession = require('../../src/backend/dbSession.js');
+
+var Server = function(port) {
+  var server = Percolator({'port': port, autoLink: false});
+
+  server.route('/api/keywords',
+
+  )
+}
