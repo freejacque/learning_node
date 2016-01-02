57d81f89666f62e32a7cc17c45eead337cc1239e
freejacque <freejacque@gmail.com>
2016-01-01 21:20:48 -0500

    removed server code from index

--- keyword-wrangler/src/backend/index.js
+++ keyword-wrangler/src/backend/index.js
@@ -1,27 +1,8 @@
 'use strict';
 
-var Percolator = require('percolator').Percolator;
-var dbSession = require('../../src/backend/dbSession.js');
+var Server = require('./server.js').Server;
 
-var port = 8080;
-var server = Percolator({'port': port, 'autoLink': false});
-
-server.route('/api/keywords',
-  {
-    GET: function(request, response) {
-      dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id',
-
-      function(err, rows) {
-        if(err) {
-          console.log(err);
-          response.status.internalServerError(err);
-        } else {
-          response.collection(rows).send();
-        }
-      });
-    }
-  }
-);
+var server = Server('8080');
 
 server.listen(function() {
   console.log('Server started and listening on port', port);
