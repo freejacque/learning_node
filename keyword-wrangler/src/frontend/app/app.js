0ede4bd99d030d42992caf7a7550c0e121725eaa
freejacque <freejacque@gmail.com>
2016-01-03 14:04:25 -0500

    added comment

--- keyword-wrangler/src/frontend/app/app.js
+++ keyword-wrangler/src/frontend/app/app.js
@@ -6,6 +6,7 @@
 
   app.config(['$routeProvider',
     function($routeProvider) {
+      // makes app/keywords/KeywordsConroller.js handle http://localhost:8080/#/URL
       $routeProvider.when('/', {
         templateUrl: 'app/keywords/partials/editor.html',
         controller: 'KeywordsController'
