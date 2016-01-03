86ea34b74b74abc838ee8060f62921995848d160
freejacque <freejacque@gmail.com>
2016-01-03 14:02:00 -0500

    created app function

--- keyword-wrangler/src/frontend/app/app.js
+++ keyword-wrangler/src/frontend/app/app.js
@@ -1,1 +1,16 @@
 'use script';
+
+(function() {
+
+  var app = angular.module('app', ['ngRoute', 'ngGrid', 'restangular']);
+
+  app.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.when('/', {
+        templateUrl: 'app/keywords/partials/editor.html',
+        controller: 'KeywordsController'
+      });
+
+    }]);
+
+})();
