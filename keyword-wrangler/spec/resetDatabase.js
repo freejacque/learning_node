26ff4f8a7576e372829717cfb561c7aa02bc72a1
freejacque <freejacque@gmail.com>
2016-01-12 18:38:45 -0500

    updated resetDB helper module to check for env

--- keyword-wrangler/spec/resetDatabase.js
+++ keyword-wrangler/spec/resetDatabase.js
@@ -1,36 +1,68 @@
 'use strict';
 
 var async = require('async');
+var env = require('../src/backend/env');
+var dbOptions = require('../database.json')[env];
 // uses a dbSession object
 var resetDatabase = function(dbSession, callback) {
 
-  async.series(
-    [
+  if(dbOptions.driver === 'sqlite3') {
 
-      function(callback) {
-        dbSession.remove('keyword', '1', function(err) {
-          callback(err)
-        });
-      },
+    async.series(
+      [
 
-      function(callback) {
-        dbSession.remove('category', '1', function(err) {
-          callback(err)
-        });
-      },
+        function(callback) {
+          dbSession.remove('keyword', '1', function(err) {
+            callback(err)
+          });
+        },
 
-      function(callback) {
-        dbSession.remove('sqlite_sequence', '1', function(err) {
-          callback(err)
-        });
+        function(callback) {
+          dbSession.remove('category', '1', function(err) {
+            callback(err)
+          });
+        },
+
+        function(callback) {
+          dbSession.remove('sqlite_sequence', '1', function(err) {
+            callback(err)
+          });
+        }
+
+      ],
+
+      function(err, results) {
+        callback(err);
       }
+    );
 
-    ],
+  }
 
-    function(err, results) {
-      callback(err);
-    }
-  );
+  if(dbOptions.driver === 'mysql') {
+
+    async.series(
+      [
+
+        function(callback) {
+          dbSession.remove('TRUNCATE keyword', [], function(err) {
+            callback(err)
+          });
+        },
+
+        function(callback) {
+          dbSession.remove('TRUNCATE category', [], function(err) {
+            callback(err)
+          });
+        }
+
+      ],
+
+      function(err, results) {
+        callback(err);
+      }
+    );
+
+  }
 
 };
 //  exports the resetDatabase function so it can be used in other files
