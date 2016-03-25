e02dc6d0993de93c1ddacaa8911e4a5a415b079d
freejacque <freejacque@gmail.com>
2016-03-24 20:54:27 -0400

    created index

---
+++ first_api/src/backend/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Server = require('./server.js').Server;
+
+var server = Server('8080');
+
+server.listen(function() {
+  console.log('Server started and listening on port', server.options.port);
+});
+
+// start the server using: KW_ENV=dev node src/backend/index.js
