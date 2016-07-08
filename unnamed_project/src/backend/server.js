b0fe376293b3753ff81875c75bdf815400c3a55f
freejacque <freejacque@gmail.com>
2016-07-07 21:29:09 -0400

    added server to the server

--- unnamed_project/src/backend/server.js
+++ unnamed_project/src/backend/server.js
@@ -2,3 +2,9 @@
 
 var Percolator = require('percolator').Percolator
 var dbSession = require('../../src/backend/dbSession.js');
+
+var Server = function(port) {
+  var server = Percolator({'port': port, 'autolink': false, 'staticDir': __dirname + '/../frontend'});
+
+
+}
