dfc2f452b1ffdf0c5c5bf79ad68127f765fcce16
freejacque <freejacque@gmail.com>
2016-07-08 22:52:04 -0400

    added get route to server

--- unnamed_project/src/backend/server.js
+++ unnamed_project/src/backend/server.js
@@ -6,5 +6,19 @@ var dbSession = require('../../src/backend/dbSession.js');
 var Server = function(port) {
   var server = Percolator({'port': port, 'autolink': false, 'staticDir': __dirname + '/../frontend'});
 
-
+  server.route('/api/unnamed'),
+  {
+    GET: function(request, response) {
+      dbSession.fetchAll('SELECT id, value FROM table ORDER BY id',
+        function(err, rows) {
+          if(err) {
+            console.log(err);
+            response.status.internalServerError(err);
+          } else {
+            response.collection(rows).send();
+          }
+        }
+      );
+    }
+  }
 }
