fbabae416f498aea263164f672639bfb74a706cd
freejacque <freejacque@gmail.com>
2016-07-13 06:05:08 -0400

    started a conditional for db drivers

--- unnamed_project/src/backend/dbSession.js
+++ unnamed_project/src/backend/dbSession.js
@@ -5,3 +5,11 @@ var dbOptions = require('../../database.json')[env];
 var DBWrapper = require('node-dbi').DBWrapper;
 
 var dbWrapper;
+
+if(dbOptions.driver === 'sqlite3') {
+
+} else if (dbOptions.driver === 'mysql') {
+
+} else {
+
+}
