f55abb861bbe9c2caf5e9583fbb9e8249bcebe1e
freejacque <freejacque@gmail.com>
2016-07-15 05:51:51 -0400

    added dbwrapper for mysql

--- unnamed_project/src/backend/dbSession.js
+++ unnamed_project/src/backend/dbSession.js
@@ -9,7 +9,12 @@ var dbWrapper;
 if(dbOptions.driver === 'sqlite3') {
   dbWrapper = new DBWrapper('sqlite3', {'path': dbOptions.filename});
 } else if (dbOptions.driver === 'mysql') {
-
+  dbWrapper = new DBWrapper('mysql', {
+    'host'    : dbOptions.host,
+    'user'    : dbOptions.user,
+    'password': dbOptions.password,
+    'database': dbOptions.database
+  });
 } else {
 
 }
