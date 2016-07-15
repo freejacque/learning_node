bc4728ece799a917cebf3e7b00cbdb29c4eea945
freejacque <freejacque@gmail.com>
2016-07-14 20:18:33 -0400

    added sqlites dbwrapper

--- unnamed_project/src/backend/dbSession.js
+++ unnamed_project/src/backend/dbSession.js
@@ -7,7 +7,7 @@ var DBWrapper = require('node-dbi').DBWrapper;
 var dbWrapper;
 
 if(dbOptions.driver === 'sqlite3') {
-
+  dbWrapper = new DBWrapper('sqlite3', {'path': dbOptions.filename});
 } else if (dbOptions.driver === 'mysql') {
 
 } else {
