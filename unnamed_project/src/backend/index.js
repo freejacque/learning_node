23f64f5dd5d53b2909a0fa901c6ff386349a289b
freejacque <freejacque@gmail.com>
2016-07-21 22:02:43 -0400

    added server.listen function

--- unnamed_project/src/backend/index.js
+++ unnamed_project/src/backend/index.js
@@ -2,3 +2,7 @@
 
 var Server = require('./server.js').Server;
 var server = Server('8080');
+
+server.listen(function() {
+  console.log('Server started and listening in port', server.options.port);
+});
