f231b81b3a9a0303d192c70b150fd1e21f3ba330
freejacque <freejacque@gmail.com>
2016-07-22 05:51:02 -0400

    added comment

--- unnamed_project/src/backend/index.js
+++ unnamed_project/src/backend/index.js
@@ -6,3 +6,5 @@ var server = Server('8080');
 server.listen(function() {
   console.log('Server started and listening in port', server.options.port);
 });
+
+// start server with: KW_ENV=dev node src/backend/index.js
